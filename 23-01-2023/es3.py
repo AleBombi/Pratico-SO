028ca456028795be1fa2c349ec4bb3432faca084
AleBombi <alessandroravveduto@gmail.com>
2023-06-09 13:58:20 +0200

    es3 completato

--- 23-01-2023/es3.py
+++ 23-01-2023/es3.py
@@ -15,4 +15,15 @@ dirpath2 = sys.argv[2]
 Files1 = [files for files in os.listdir(dirpath1) if os.path.isfile(os.path.join(dirpath1, files))]
 Files2 = [files for files in os.listdir(dirpath2) if os.path.isfile(os.path.join(dirpath2, files))]
 
-CommonFiles = Files1 & Files2
+CommonFiles = [files for files in Files1 if files in Files2]
+
+for file in CommonFiles:
+    with open(os.path.join(os.path.join(curPath, dir3), file), "x") as f:
+         with open(os.path.join(dirpath1, file), "r") as f2c:
+             f.write(f2c.read())
+
+for file in CommonFiles:
+    with open(os.path.join(os.path.join(curPath, dir4), file), "x") as f:
+         with open(os.path.join(dirpath2, file), "r") as f2c:
+             f.write(f2c.read())
+
